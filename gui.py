a621de4ce5dd254a456ee64406fd8b3fe2ff2f5c
mpmoran2 <kamokumoran@gmail.com>
2021-06-14 12:07:54 -0700

    Download button functional

--- gui.py
+++ gui.py
@@ -1,5 +1,14 @@
 from tkinter import *
 from tkinter import filedialog
+from pytube import YouTube
+from moviepy.editor import *
+
+def download():
+    video_path = url_entry.get()
+    file_path = path_label.cget("text")
+    mp4 = YouTube(video_path).streams.get_highest_resolution().download()
+    vid_clip = VideoFileClip(mp4)
+    vid_clip.close()
 
 def get_path():
     path = filedialog.askdirectory()
@@ -31,7 +40,7 @@ canvas.create_window(200,150,window=path_label)
 canvas.create_window(200,175,window=path_button)
 
 # button
-download_button = Button(root, text="Download")
+download_button = Button(root, text="Download",command=download)
 canvas.create_window(200,250,window=download_button)
 
 root.mainloop()
